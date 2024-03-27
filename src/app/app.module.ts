d3c04260d0bad1aec87c8c736eea61e151dcda57
Tkarkib Lhassan <73590332+LhassanTkarkib@users.noreply.github.com>
2024-03-27 22:24:50 +0000

    BT-8 setting up dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {AngularToastifyModule} from "angular-toastify";
+import {ChartsModule} from "ng2-charts";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularToastifyModule,
+    ChartsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
